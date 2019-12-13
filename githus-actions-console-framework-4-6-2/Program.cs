3e0c1529f59d9fc2e579d8466b26b1f0c88a0bd8
Michell Cronberg <michell@cronberg.dk>
2019-12-13 09:47:15 +0100

    Add project files.

---
+++ githus-actions-console-framework-4-6-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githus_actions_console_framework_4_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
